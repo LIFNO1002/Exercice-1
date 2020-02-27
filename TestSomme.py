80539ba3ff4ace6f4d3fae53aaeccc8c88975c32
FlorentinBUCL <61228055+FlorentinBUCL@users.noreply.github.com>
2020-02-27 17:39:08 +0100

    Fichier final de test retenu pour la remise du 2/3

--- TestSomme.py
+++ TestSomme.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
+import unittest
+import sys
+
+import CorrSomme as correct
+import sommetest as student
+
+
+class TestSomme(unittest.TestCase):
+
+    def test1_sommetest(self):
+        args = [2, 5, 20, 100]
+        rep = _(
+            "Votre fonction a retourné {} lorsqu'elle est appelée avec {} comme argument alors que la reponse attendue est {}")
+        for n in args:
+            try:
+                student_ans = student.somme_n_premiers_nbrs(n)
+            except Exception as e:
+                self.fail("Votre fonction a provoqué l'erreur {}: {} avec comme argument {}".format(type(e), e, n))
+            correct_ans = correct.somme_n_premiers_nbrs(n)
+            self.assertEqual(student_ans, correct_ans, rep.format(student_ans, n, correct_ans))
+
+
+if __name__ == '__main__':
+    unittest.main()
